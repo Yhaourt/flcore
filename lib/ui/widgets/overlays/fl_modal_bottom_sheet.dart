bc72f0e95d5e670223f36ca7c12606a7728d3512
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2024-07-29 11:42:51 +0200

    fix: modal bottom sheet scroll

--- lib/ui/widgets/overlays/fl_modal_bottom_sheet.dart
+++ lib/ui/widgets/overlays/fl_modal_bottom_sheet.dart
@@ -36,62 +36,60 @@ class FlModalBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
+    return Container(
+      width: double.infinity,
       constraints: BoxConstraints(
         maxHeight: maxHeight ?? MediaQuery.of(context).size.height * 0.8,
       ),
-      child: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.only(
-            top: FlTheme.spacing,
-            bottom: MediaQuery.of(context).viewInsets.bottom + FlTheme.padding,
-            left: FlTheme.spacing,
-            right: FlTheme.spacing,
-          ),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(FlTheme.borderRadius),
+      padding: EdgeInsets.only(
+        top: FlTheme.spacing,
+        bottom: MediaQuery.of(context).viewInsets.bottom + FlTheme.padding,
+        left: FlTheme.spacing,
+        right: FlTheme.spacing,
+      ),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(FlTheme.borderRadius),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 7,
+              width: 50,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(FlTheme.borderRadius),
+              ),
             ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: 7,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(FlTheme.borderRadius),
+          SizedBox(height: FlTheme.spacingLg),
+          if (title != null) ...[
+            Text(
+              title!,
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    fontWeight: FontWeight.bold,
                   ),
-                ),
+            ),
+            if (subtitle != null)
+              Text(
+                subtitle!,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
               ),
-              SizedBox(height: FlTheme.spacingLg),
-              if (title != null) ...[
-                Text(
-                  title!,
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                ),
-                if (subtitle != null)
-                  Text(
-                    subtitle!,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                SizedBox(height: FlTheme.spacingLg),
-              ],
-              Padding(
-                padding: EdgeInsets.all(FlTheme.padding),
-                child: child,
-              ),
-            ],
+            SizedBox(height: FlTheme.spacingLg),
+          ],
+          Padding(
+            padding: EdgeInsets.all(FlTheme.padding),
+            child: SingleChildScrollView(
+              child: child,
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
