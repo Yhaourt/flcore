8a58e7a473333e7aa449bc6107fac88d0a4564fe
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2024-07-29 11:39:34 +0200

    feat: fl_modal_bottom_sheet maxHeight

--- lib/ui/widgets/overlays/fl_modal_bottom_sheet.dart
+++ lib/ui/widgets/overlays/fl_modal_bottom_sheet.dart
@@ -6,12 +6,14 @@ showFlModalBottomSheet(
   required Widget child,
   String? title,
   String? subtitle,
+  double? maxHeight,
 }) {
   showModalBottomSheet(
     context: context,
     builder: (context) => FlModalBottomSheet(
       title: title,
       subtitle: subtitle,
+      maxHeight: maxHeight,
       child: child,
     ),
     isScrollControlled: true,
@@ -24,64 +26,71 @@ class FlModalBottomSheet extends StatelessWidget {
     required this.child,
     this.title,
     this.subtitle,
+    this.maxHeight,
   });
 
   final Widget child;
   final String? title;
   final String? subtitle;
+  final double? maxHeight;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.only(
-          top: FlTheme.spacing,
-          bottom: MediaQuery.of(context).viewInsets.bottom + FlTheme.padding,
-          left: FlTheme.spacing,
-          right: FlTheme.spacing,
-        ),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surface,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(FlTheme.borderRadius),
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: maxHeight ?? MediaQuery.of(context).size.height * 0.8,
+      ),
+      child: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(
+            top: FlTheme.spacing,
+            bottom: MediaQuery.of(context).viewInsets.bottom + FlTheme.padding,
+            left: FlTheme.spacing,
+            right: FlTheme.spacing,
           ),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 7,
-                width: 50,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(FlTheme.borderRadius),
-                ),
-              ),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(FlTheme.borderRadius),
             ),
-            SizedBox(height: FlTheme.spacingLg),
-            if (title != null) ...[
-              Text(
-                title!,
-                style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              if (subtitle != null)
-                Text(
-                  subtitle!,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                  textAlign: TextAlign.center,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 7,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(FlTheme.borderRadius),
+                  ),
                 ),
+              ),
               SizedBox(height: FlTheme.spacingLg),
+              if (title != null) ...[
+                Text(
+                  title!,
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                if (subtitle != null)
+                  Text(
+                    subtitle!,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                SizedBox(height: FlTheme.spacingLg),
+              ],
+              Padding(
+                padding: EdgeInsets.all(FlTheme.padding),
+                child: child,
+              ),
             ],
-            Padding(
-              padding: EdgeInsets.all(FlTheme.padding),
-              child: child,
-            ),
-          ],
+          ),
         ),
       ),
     );
