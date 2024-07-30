5ecb268c49b4bf7f0deb04b77df6db24a33c5697
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2024-07-30 14:09:56 +0200

    feat: fl_app changeNotifierProviders

--- lib/core/fl_app.dart
+++ lib/core/fl_app.dart
@@ -2,6 +2,7 @@ import 'package:flcore/core/routing/fl_router.dart';
 import 'package:flcore/ui/theme/fl_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 class FlApp extends StatelessWidget {
   const FlApp({
@@ -9,42 +10,48 @@ class FlApp extends StatelessWidget {
     required this.title,
     required this.router,
     required this.theme,
-    this.providers = const <BlocProvider>[],
+    this.changeNotifierProviders = const <ChangeNotifierProvider>[],
+    this.blocProviders = const <BlocProvider>[],
   });
 
   final String title;
   final FlRouter router;
   final FlTheme theme;
-  final List<BlocProvider> providers;
+  final List<ChangeNotifierProvider> changeNotifierProviders;
+  final List<BlocProvider> blocProviders;
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: providers,
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: title,
-        restorationScopeId: '${title.toLowerCase()}-app',
-        themeMode: ThemeMode.light,
-        theme: theme.lightTheme(),
-        darkTheme: theme.darkTheme(),
-        routerConfig: router.router,
-        builder: (context, child) {
-          return Overlay(
-            initialEntries: [
-              OverlayEntry(
-                builder: (context) {
-                  return GestureDetector(
-                    onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-                    onPanDown: (_) =>
-                        FocusManager.instance.primaryFocus?.unfocus(),
-                    child: child!,
-                  );
-                },
-              ),
-            ],
-          );
-        },
+    return MultiProvider(
+      providers: changeNotifierProviders,
+      child: MultiBlocProvider(
+        providers: blocProviders,
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          restorationScopeId: '${title.toLowerCase()}-app',
+          themeMode: ThemeMode.light,
+          theme: theme.lightTheme(),
+          darkTheme: theme.darkTheme(),
+          routerConfig: router.router,
+          builder: (context, child) {
+            return Overlay(
+              initialEntries: [
+                OverlayEntry(
+                  builder: (context) {
+                    return GestureDetector(
+                      onTap: () =>
+                          FocusManager.instance.primaryFocus?.unfocus(),
+                      onPanDown: (_) =>
+                          FocusManager.instance.primaryFocus?.unfocus(),
+                      child: child!,
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
