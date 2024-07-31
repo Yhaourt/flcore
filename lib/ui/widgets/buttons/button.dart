c778490e28d410e06663786616204dc92891ad2e
Yhaourt <84399858+KoroSai46@users.noreply.github.com>
2024-07-31 11:17:57 +0200

    fix: button

--- lib/ui/widgets/buttons/button.dart
+++ lib/ui/widgets/buttons/button.dart
@@ -5,21 +5,21 @@ import 'package:flcore/ui/theme/fl_theme.dart';
 class Button extends StatelessWidget {
   const Button({
     super.key,
-    required this.text,
+    this.text,
     this.icon,
     required this.onPressed,
+    this.backgroundColor,
     this.expanded = false,
     this.thin = false,
-    this.borderRadius,
     this.isLoading = false,
   });
 
-  final String text;
-  final VoidCallback onPressed;
+  final String? text;
   final IconData? icon;
+  final VoidCallback onPressed;
+  final Color? backgroundColor;
   final bool expanded;
   final bool thin;
-  final double? borderRadius;
   final bool isLoading;
 
   @override
@@ -29,7 +29,8 @@ class Button extends StatelessWidget {
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           fixedSize: Size.fromHeight(thin ? 38 : 50),
-          backgroundColor: Theme.of(context).colorScheme.primary,
+          backgroundColor:
+              backgroundColor ?? Theme.of(context).colorScheme.primary,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(FlTheme.borderRadius),
           ),
@@ -48,14 +49,15 @@ class Button extends StatelessWidget {
                       icon,
                       color: Theme.of(context).colorScheme.onPrimary,
                     ),
-                    const SizedBox(width: FlTheme.spacing),
+                    if (text != null) const SizedBox(width: FlTheme.spacing),
                   ],
-                  Text(
-                    text,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary,
-                        ),
-                  ),
+                  if (text != null)
+                    Text(
+                      text!,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                    ),
                 ],
               ),
       ),
