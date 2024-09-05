81de9b0246e077957b232fd735e94bed1d577bb8
Yhaourt <mattis.crouzet@outlook.fr>
2024-09-05 20:49:04 +0200

    feat: di env variables accessible from configure and finalize callbacks

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -1,18 +1,22 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 
 final di = GetIt.instance;
 
 class DependencyInjection {
   static Future<void> init({
-    required Function(GetIt) configure,
-    Function(GetIt)? finalize,
+    required Function(GetIt, Map<String, String> env) configure,
+    Function(GetIt, Map<String, String>)? finalize,
+    bool loadEnv = true,
+    String envFile = '.env',
   }) async {
-    configure(di);
+    if (loadEnv) await dotenv.load(fileName: envFile);
+    configure(di, dotenv.env);
 
     await di.allReady(
       timeout: const Duration(seconds: 5),
     );
 
-    if (finalize != null) finalize(di);
+    if (finalize != null) finalize(di, dotenv.env);
   }
 }
