1df8abb088b33735826bdad0797cc2aeefccf729
Yhaourt <mattis.crouzet@outlook.fr>
2024-09-06 19:20:35 +0200

    doc: provider

--- lib/core/provider/provider.dart
+++ lib/core/provider/provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+/// A class that provides a stream of data.
 abstract class Provider<T> {
   Provider({
     required this.load,
@@ -7,42 +8,48 @@ abstract class Provider<T> {
   }) {
     _streamController = StreamController<T>.broadcast(
       onListen: () => (broadcastOnListen)
-          ? (loaded)
+          ? (_loaded)
               ? broadcast(data)
               : _loadAndBroadcast()
           : null,
     );
   }
 
+  /// Function that loads the data.
   final Future<T> Function() load;
 
-  bool loaded = false;
+  bool _loaded = false;
 
   T? _data;
 
+  /// Current data.
+  /// Throws an exception if the provider is not loaded yet.
   T get data {
-    if (!loaded) throw Exception('Provider not loaded yet');
+    if (!_loaded) throw Exception('Provider not loaded yet');
     return _data!;
   }
 
   late final StreamController<T> _streamController;
 
+  /// Stream of data.
   Stream<T> get stream => _streamController.stream;
 
   Future<void> _loadAndBroadcast() async {
     try {
       broadcast(await load());
-      loaded = true;
+      _loaded = true;
     } catch (e) {
       _streamController.addError(e);
     }
   }
 
+  /// Broadcasts the data.
   void broadcast(T data) {
     _data = data;
     _streamController.add(data);
   }
 
+  /// Disposes the stream.
   void dispose() {
     _streamController.close();
   }
