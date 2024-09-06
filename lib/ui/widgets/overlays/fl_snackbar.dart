030e5082688370ef8d736966bcc0b963a50c50ed
Yhaourt <mattis.crouzet@outlook.fr>
2024-09-06 19:49:28 +0200

    feat: flSnackBar

---
+++ lib/ui/widgets/overlays/fl_snackbar.dart
@@ -0,0 +1,28 @@
+import 'package:flcore/flcore.dart';
+import 'package:flutter/material.dart';
+
+showFlSnackBar(
+    BuildContext context,
+    String message, {
+      Color? backgroundColor,
+      Color? textColor,
+    }) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        message,
+        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+          color: textColor ?? Theme.of(context).colorScheme.onSecondary,
+        ),
+      ),
+      backgroundColor:
+      backgroundColor ?? Theme.of(context).colorScheme.secondary,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(FlTheme.borderRadius),
+          topRight: Radius.circular(FlTheme.borderRadius),
+        ),
+      ),
+    ),
+  );
+}
