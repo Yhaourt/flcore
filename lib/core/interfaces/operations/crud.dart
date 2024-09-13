2f8a1d62e663f5aa902c72f79564007878fecb99
Yhaourt <mattis.crouzet@amediasolutions.fr>
2024-09-13 14:57:56 +0200

    fix: crud operations mixins to abstract classes

--- lib/core/interfaces/operations/crud.dart
+++ lib/core/interfaces/operations/crud.dart
@@ -1,23 +1,23 @@
-mixin FindAll<T> {
+abstract class FindAll<T> {
   Future<List<T>> findAll();
 }
 
-mixin Find<T> {
+abstract class Find<T> {
   Future<T> find(String id);
 }
 
-mixin Insert<T> {
+abstract class Insert<T> {
   Future<void> insert(T entity);
 }
 
-mixin Update<T> {
+abstract class Update<T> {
   Future<void> update(T entity);
 }
 
-mixin Upsert<T> {
+abstract class Upsert<T> {
   Future<void> upsert(T entity);
 }
 
-mixin Delete<T> {
+abstract class Delete<T> {
   Future<void> delete(T entity);
 }
